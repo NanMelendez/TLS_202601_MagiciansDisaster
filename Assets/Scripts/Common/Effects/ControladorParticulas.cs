13bb95c096ba9e1cf738c765b519725ecae0f4d7
Facugod749 <facureyesdelacolina@gmail.com>
2026-05-28 17:53:47 -0500

    Prueba de funcionamiento de las particulas

--- Assets/Scripts/Common/Effects/ControladorParticulas.cs
+++ Assets/Scripts/Common/Effects/ControladorParticulas.cs
@@ -55,6 +55,7 @@ public class ControladorParticulas : MonoBehaviour
             Destroy(gameObject);
             Debug.Log("Objeto de particulas eliminado");
         }
+       
     }
 }
             
